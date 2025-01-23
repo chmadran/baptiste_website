ed064a736abb257cb555e273cfc9c158adbf9ab1
chmadran <charlotte.madrangeas@gmail.com>
2025-01-23 17:41:58 +0100

    victime

--- src/Components/PhaseMisenCause.js
+++ src/Components/PhaseMisenCause.js
@@ -1,15 +1,97 @@
-import './PhaseMisenCause.css'
+import './PhaseMisenCause.css';
 
 const PhaseMisenCause = () => {
   return (
     <div className="mis-en-cause-container">
-        <div className="title-section">
-          <h1>Je suis mis en cause</h1>
-          <div className="title-underline"></div>
-        </div>
+      <div className="content">
+      <h2 className="highlighted-title">L’un de vos proches a été placé en garde à vue ? Vous êtes convoqué à une audience correctionnelle ? Vous êtes accusé d’un crime ?</h2>
+        <h2 className="highlighted-title">Maître Baptiste Grimaud vous défendra rapidement et efficacement pour faire valoir vos droits.</h2>
+        <section>
+          <h3>Garde à vue :</h3>
+          <p>
+            La garde à vue est une mesure de privation de liberté, prise par un officier de police judiciaire sous le contrôle de l’autorité judiciaire.
+            Cette mesure est prise à l’encontre de personne contre laquelle il existe « une ou plusieurs raisons plausibles de soupçonner qu’elle a commis ou tenté de commettre un crime ou un délit ».
+          </p>
+          <p>
+            La garde à vue permet aux enquêteurs de maintenir la personne soupçonnée dans leurs locaux pour une durée d’au moins 24h, d’auditionner la personne gardée à vue ou de procéder à tout acte permettant de rassembler des éléments utiles pour l’enquête.
+          </p>
+          <p>
+            La personne gardée à vue doit se voir notifier ses droits, notamment le droit de se voir assister d’un avocat et de s’entretenir confidentiellement avec ce dernier pour une durée de 30 minutes par tranche de 24h de garde à vue.
+          </p>
+          <p>
+            Si les droits du gardé à vue ne sont pas respectés durant le temps de la garde à vue, l’intervention d’un avocat est indispensable pour contester la régularité de la mesure en soulevant un vice de procédure.
+          </p>
+          <p>Pour une intervention rapide, n’hésitez pas à contacter Maître Baptiste Grimaud en urgence, qui sera disponible 24h/24.</p>
+        </section>
+        <section>
+          <h3>Comparution immédiate :</h3>
+          <p>
+            La procédure de comparution immédiate permet de juger un prévenu dans des délais très courts, l’objectif de l’autorité judiciaire étant d’apporter une réponse rapide et ferme aux faits commis.
+          </p>
+          <p>
+            La violence de cette procédure accélérée réside dans l’enchaînement des évènements. En effet, à l’issue de sa garde à vue, le mis en cause est déféré devant le Procureur de la République puis jugé par un Tribunal composé de trois magistrats.
+          </p>
+          <p>
+            Au vu de l’urgence de la situation, l’intervention d’un avocat est indispensable pour assurer la défense du prévenu et pour veiller au respect de ses droits durant la procédure.
+          </p>
+          <p>
+            Maître Baptiste Grimaud veillera au respect de vos droits durant la procédure et assurera votre défense en mettant en place une stratégie personnalisée et une argumentation solide.
+          </p>
+        </section>
+        <section>
+          <h3>Détention provisoire :</h3>
+          <p>
+            La détention provisoire est une mesure de privation de liberté permettant à l’autorité judiciaire de maintenir la personne à la disposition de la justice, d’éviter toute tentative de fuite ou encore de prévenir le renouvellement de l’infraction.
+          </p>
+          <p>Deux situations sont à distinguer en cas de placement en détention provisoire :</p>
+          <ul>
+            <li>La personne est incarcérée dans l’attente de son jugement.</li>
+            <li>La personne est incarcérée dans le cadre d’une information judiciaire, après avoir été mis en examen.</li>
+          </ul>
+          <p>
+            Ces deux cas nécessitent l’intervention d’un avocat afin de présenter à la juridiction compétente des garanties de représentation solides afin de mettre fin à la détention provisoire et obtenir une remise en liberté sous contrôle judiciaire.
+          </p>
+          <p>Prenez attache avec le Cabinet dans les meilleurs délais afin de préparer une demande de remise en liberté.</p>
+        </section>
+        <section>
+          <h3>Convocation devant le Tribunal correctionnel :</h3>
+          <p>
+            Être convoqué devant une juridiction répressive est un moment décisif qui détermine les mois ou années à venir selon la peine prononcée. Les enjeux d'une procédure pénale sont donc si considérables qu’il est crucial d'être accompagné par un avocat pénaliste.
+          </p>
+          <p>
+            Ainsi, le Cabinet se chargera d’obtenir le dossier complet afin de l'examiner minutieusement, tant sur le fond que sur la procédure.
+          </p>
+          <p>
+            En amont, des rendez-vous de préparation seront organisés pour préparer votre défense et rassembler les pièces qui seront produites à votre bénéfice.
+          </p>
+          <p>
+            Le jour de l’audience, Maître Baptiste Grimaud assurera la défense de vos intérêts et développera, face aux juges, un argumentaire solide.
+          </p>
+          <p>Si vous êtes convoqué devant le Tribunal correctionnel, je vous recommande de prendre contact avec le Cabinet sans délai.</p>
+        </section>
+        <section>
+          <h3>Comparution sur reconnaissance préalable de culpabilité (CRPC) :</h3>
+          <p>
+            La comparution sur reconnaissance préalable de culpabilité, dite CRPC, est un mode de poursuite simplifié qui nécessite une reconnaissance des faits reprochés.
+          </p>
+          <p>L’assistance d’un avocat est obligatoire.</p>
+          <p>Cette procédure se déroule en deux étapes :</p>
+          <ul>
+            <li>
+              L’étape de la proposition de peine avec le Procureur de la République est une phase qui consiste en une négociation entre le magistrat et le prévenu, assisté de son avocat, sur le choix de la peine et son quantum (emprisonnement avec sursis, inscription au casier judiciaire B2, amende,…).
+            </li>
+            <li>
+              L’étape de l’homologation par un Juge du siège dans laquelle le juge décide de valider ou non la proposition de peine précédemment négociée.
+            </li>
+          </ul>
+          <p>
+            Par conséquent, le rôle de l’avocat est essentiel puisqu’il permet d’étudier la procédure, de négocier une peine adaptée à votre situation et à vos contraintes, et de vous informer de l’opportunité d’accepter cette peine ou, au contraire, se rendre à une audience pour contester le dossier.
+          </p>
+          <p>Si vous êtes convoqué en CRPC, je vous recommande de prendre contact avec le Cabinet sans délai.</p>
+        </section>
+      </div>
     </div>
   );
-}
+};
 
 export default PhaseMisenCause;
-
--- src/Components/PhaseVictime.js
+++ src/Components/PhaseVictime.js
@@ -4,11 +4,10 @@ const PhaseVictime = () => {
   return (
     <div className="victime-container">
         <div className="victime-title-section">
-          <h1>Vous avez été victime d'une infraction ?</h1>
-          <div className="victime-title-underline"></div>
+          <h2>Vous avez été victime d'une infraction ?</h2>
+          <h2>Maître Baptiste Grimaud vous assiste à tous les stades de la procédure.</h2>
         </div>
           <div className="victime-text-section">
-          <h2>Maître Baptiste Grimaud vous assiste à tous les stades de la procédure.</h2>
           <p>Le Cabinet <strong>assiste les victimes d’infraction à tous les stades de la procédure pénale</strong> : dès le moment du dépôt de plainte, pendant l’enquête et lors de l’audience correctionnelle pour obtenir réparation des différents préjudices.</p> 
           <p>En cas d’ouverture d’une information judiciaire, Maître Baptiste Grimaud assiste les <strong>parties civiles</strong> lors de toute la procédure d’instruction, notamment pour formuler des demandes d’actes utiles à la manifestation de la vérité auprès du Juge d’instruction, ainsi que pour faire <strong>porter leur voix</strong> et <strong>garantir une juste indemnisation de leur préjudice devant toutes les juridictions de jugement</strong> (Tribunal correctionnel, Cour d’assises, Cour criminelle départementale).</p>
           <p>En outre, le Cabinet intervient <strong>auprès des instances permettant l’indemnisation des victimes d’infractions</strong> (CIVI, SARVI) pour solliciter la réparation des dommages subis.</p>
