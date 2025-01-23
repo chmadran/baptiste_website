f5268b0d0e498add34d4d11c7d1c53cea0509d9b
chmadran <charlotte.madrangeas@gmail.com>
2025-01-23 17:05:13 +0100

    victime text

--- src/Components/PhaseVictime.js
+++ src/Components/PhaseVictime.js
@@ -2,12 +2,18 @@ import './PhaseVictime.css'
 
 const PhaseVictime = () => {
   return (
-    <div className="mis-en-cause-container">
-        <div className="title-section">
-          <h1>Je suis victime d'une infraction</h1>
-          <div className="title-underline"></div>
+    <div className="victime-container">
+        <div className="victime-title-section">
+          <h1>Vous avez été victime d'une infraction ?</h1>
+          <div className="victime-title-underline"></div>
         </div>
-    </div>
+          <div className="victime-text-section">
+          <h2>Maître Baptiste Grimaud vous assiste à tous les stades de la procédure.</h2>
+          <p>Le Cabinet <strong>assiste les victimes d’infraction à tous les stades de la procédure pénale</strong> : dès le moment du dépôt de plainte, pendant l’enquête et lors de l’audience correctionnelle pour obtenir réparation des différents préjudices.</p> 
+          <p>En cas d’ouverture d’une information judiciaire, Maître Baptiste Grimaud assiste les <strong>parties civiles</strong> lors de toute la procédure d’instruction, notamment pour formuler des demandes d’actes utiles à la manifestation de la vérité auprès du Juge d’instruction, ainsi que pour faire <strong>porter leur voix</strong> et <strong>garantir une juste indemnisation de leur préjudice devant toutes les juridictions de jugement</strong> (Tribunal correctionnel, Cour d’assises, Cour criminelle départementale).</p>
+          <p>En outre, le Cabinet intervient <strong>auprès des instances permettant l’indemnisation des victimes d’infractions</strong> (CIVI, SARVI) pour solliciter la réparation des dommages subis.</p>
+          </div>
+      </div>
   );
 }
 
