d3e45a46eed1abb78878ca1d9501edcec87548b1
chmadran <charlotte.madrangeas@gmail.com>
2025-01-26 10:50:40 +0100

    honoraires

--- src/Components/Honoraires.js
+++ src/Components/Honoraires.js
@@ -3,11 +3,11 @@ import './Honoraires.css'
 const Honoraires = () => {
   return (
     <div className="honoraires-container">
-        <div className="title-section">
+        <div className="honoraires-title-section">
           <h1>Honoraires</h1>
-          <div className="title-underline"></div>
+          <div className="honoraires-title-underline"></div>
         </div>
-        <div className="text-section">
+        <div className="honoraires-text-section">
           <p>Le Règlement Intérieur National de la profession d’Avocat, en son article 11, fixe des règles précises en matière d’honoraires : </p>
           <p>« Les honoraires sont fixés selon les usages, en fonction de la situation de fortune du client, de la difficulté de l’affaire, des frais exposés par l’avocat, de sa notoriété et des diligences de celui-ci. L’avocat chargé d’un dossier peut demander des honoraires à son client même si ce dossier lui est retiré avant sa conclusion, dans la mesure du travail accompli ».</p>
           <p>Dans une logique de totale transparence, les honoraires sont fixés dès le premier rendez-vous en concertation avec les clients et aucune facturation n’a lieu sans en avoir été discutée au préalable.</p>
